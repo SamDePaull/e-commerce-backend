567d114cdbd4afd89818f74553df84b5fb858f42
SamDePaull <prima2100016041@webmail.uad.ac.id>
2024-07-26 16:50:41 +0700

    admincon

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -2,13 +2,23 @@
 const Product = require('../models/product');
 const Order = require('../models/order');
 
+// controllers/adminController.js
+const Product = require('../models/product');
+
 exports.manageProducts = async (req, res, next) => {
     try {
-        // Implementasi logika untuk mengelola produk
+        console.log('Request received at manageProducts');
+        const { name, price, stock } = req.body;
+        console.log('Creating product with data:', { name, price, stock });
+        const product = await Product.create({ name, price, stock });
+        console.log('Product created:', product);
+        res.status(201).json({ message: 'Product created!', product });
     } catch (error) {
+        console.error('Error in manageProducts:', error);
         next(error);
     }
 };
+
 
 exports.manageOrders = async (req, res, next) => {
     try {
